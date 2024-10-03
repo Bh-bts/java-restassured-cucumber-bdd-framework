8be162b7e48e2474942750a419691a56eefbb5de
bh-bts <thumarbhavin.786@gmail.com>
2024-10-03 15:22:41 +0530

    Add logger in test case

--- src/test/java/com/api/hooks/Hooks.java
+++ src/test/java/com/api/hooks/Hooks.java
@@ -1,25 +1,16 @@
 package com.api.hooks;
 
-import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-
 public class Hooks {
 
-    //public Logger logger = LogManager.getLogManager().getLogger(String.valueOf(Hooks.class));
-    //public Logger logger = (Logger) LoggerFactory.getLogger(Hooks.class);
-   // public Logger logger = Logger.getLogger(String.valueOf(Hooks.class));
-    public Logger logger = Logger.getLogger("devpinoyLogger");
-
+    private static final Logger logger = LogManager.getLogger(Hooks.class);
 
     @BeforeSuite
     public void beforeScenario() {
-       // PropertyConfigurator.configure("src/test/resources/log4j2.properties");
         logger.info("Starting scenario");
     }
 
--- src/test/java/com/api/stepdefinitions/ApiStepDefinitions.java
+++ src/test/java/com/api/stepdefinitions/ApiStepDefinitions.java
@@ -6,23 +6,41 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiStepDefinitions {
-    private RequestSpecification request;
     private Response response;
     private final ConfigLoader configLoader = new ConfigLoader();
+    private static final Logger logger = LogManager.getLogger(ApiStepDefinitions.class);
 
     @Given("User set the API base URL")
     public void user_set_the_api_base_url() {
+        logger.info("Setting API base URL");
+        File logFile = new File("logs/LogsGenerated.log");
+
+        if (logFile.exists()) {
+            try {
+                new FileOutputStream(logFile, false).close(); // Clear the log file
+                logger.info("Cleared previous log file.");
+            } catch (IOException e) {
+                logger.error("Failed to clear log file.", e);
+            }
+        }
+
         String baseUrl = configLoader.getBaseUrl();
         if (baseUrl == null) {
-            throw new RuntimeException("Base Url not found");
+            logger.error("Base URL not found");
         }
         RestAssured.baseURI = baseUrl;
+        logger.info("API Base URL set to: " + baseUrl);
     }
 
     @When("User send a GET request to {string}")
